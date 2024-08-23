d091ce33f678861a349f7bc1aa1d36878fc96e16
rafaelcerq28 <rafaelcerq28@gmail.com>
2024-08-24 00:56:14 +0100

    adding filters

--- rangeSearch/rageSearch.java
+++ rangeSearch/rageSearch.java
@@ -6,9 +6,11 @@ public class rageSearch {
 
 
     public static void main(String[] args) {
+        //valores de latitude e longitude
         double lon = 53.35804;
         double lat = -6.24800;
 
+        //calcula as novas lat e long de acordo com a porcentagem
         double newLonPlus = ((lon/100) * 0.003) + lon;
         double newLonMinus = ((lon/100) * 0.003);
         newLonMinus = lon - newLonMinus;
@@ -18,18 +20,21 @@ public class rageSearch {
         double newLatPlus = ((lat/100) * 0.04);
         newLatPlus = lat - newLatPlus;
 
+        //BigDecimal armazena os lats e longs 
         BigDecimal bdLonPlus = new BigDecimal(newLonPlus).setScale(5, RoundingMode.HALF_UP);
         BigDecimal bdLonMinus = new BigDecimal(newLonMinus).setScale(5, RoundingMode.HALF_UP);
 
         BigDecimal bdLatMinus = new BigDecimal(newLatMinus).setScale(5, RoundingMode.HALF_UP);
         BigDecimal bdLatPlus = new BigDecimal(newLatPlus).setScale(5, RoundingMode.HALF_UP);
 
+        //converte o bigDecimal pra double e salva nas variaveis
         newLonPlus = bdLonPlus.doubleValue();
         newLonMinus = bdLonMinus.doubleValue();
 
         newLatMinus = bdLatMinus.doubleValue();
         newLatPlus = bdLatPlus.doubleValue();
 
+        //imprime os valores
         System.out.println("Old: [" + lon + ", " + lat+"]");
         System.out.println("New Lon+:  [" + newLonPlus + ", " + lat+"]");
         System.out.println("New Lat-: [" + lon + ", " + newLatMinus+"]");
@@ -39,6 +44,6 @@ public class rageSearch {
 
         System.out.println("cruz montada");
 
-        
+
     }
 }
